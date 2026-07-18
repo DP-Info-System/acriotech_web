7ba3b41e6b5d589805474ede836af60852224eef
Darpan Patel <darpan.patelm@gmail.com>
2026-07-18 09:59:23 +0530

    fix: add 301 redirect from www to non-www to solve duplicate content SEO issue

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,22 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
       }
     ]
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.acriotech.com',
+          },
+        ],
+        destination: 'https://acriotech.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
